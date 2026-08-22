fac68ac5bc09b05412dc903dd1c28cdc2b62aeb7
Joshuagpt <ca0g2m0@gmail.com>
2026-08-22 16:22:13 +0800

    Update DllMain.cpp

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -4,10 +4,6 @@
 // GNU Lesser General Public License as published by the Free Software Foundation, either version 3
 // of the License, or (at your option) any later version.
 //
-// VanillaHelpers is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
 // You should have received a copy of the GNU Lessed General Public License along with
 // VanillaHelpers. If not, see <https://www.gnu.org/licenses/>.
 
